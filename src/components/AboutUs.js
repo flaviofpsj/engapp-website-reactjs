b7fd6ffcdd4726530c920e2b14bac7064d69b249
Flavio Farias <flavio@engapp.com.br>
2018-11-10 19:04:15 -0300

    create aboutus component

---
+++ src/components/AboutUs.js
@@ -0,0 +1,165 @@
+import React, { Component } from 'react';
+
+class AboutUs extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div id='sobre' className='team-boxed'>
+          <div className='container'>
+            <h2 className='text-center' data-aos='fade-up'>A Empresa</h2>
+            <p className='text-center' data-aos='fade-up'>A EngApp é uma empresa de inovação digital e desenvolvimento de software ágil. Combinamos processos de inovação, design de UX, tecnologia de ponta e visão de negócios para criar software que reposiciona os nossos clientes no mundo dos negócios digitais.</p>
+            <h2 className='text-center' data-aos='fade-up'>Membros</h2>
+            <div className='row people'>
+              <div className='col-md-6 col-lg-4 item' data-aos='fade-up'>
+                <div className='box'>
+                  <img className='rounded-circle' src={require('./../img/luccas.png')} />
+                  <h3 className='name'>Luccas Augusto</h3>
+                  <p className='title'>Diretor Executivo</p>
+                  <div className='social'>
+                    <a href='https://www.facebook.com/luccasskywalker' target='_blank'>
+                      <i className='fab fa-facebook-f'></i>
+                    </a>
+                    <a href='https://www.instagram.com/luccascunhasilva' target='_blank'>
+                      <i className='fab fa-instagram'></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/luccasaugusto' target='_blank'>
+                      <i className='fab fa-linkedin-in'></i>
+                    </a>
+                    <a href='https://github.com/LuccasAugusto' target='_blank'>
+                      <i className='fab fa-github'></i>
+                    </a>
+                    <a target='_blank'>
+                      <i className='fab fa-medium-m'></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-6 col-lg-4 item' data-aos='fade-up'>
+                <div className='box'>
+                  <img className='rounded-circle' src={require('./../img/aurelio.png')} />
+                  <h3 className='name'>Aurélio Buarque</h3>
+                  <p className='title'>Diretor de Tecnologia</p>
+                  <div className='social'>
+                    <a href='https://www.facebook.com/aurelio.buarque' target='_blank'>
+                      <i className='fab fa-facebook-f'></i>
+                    </a>
+                    <a href='https://www.instagram.com/abuarquemf' target='_blank'>
+                      <i className='fab fa-instagram'></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/aurelio-buarque' target='_blank'>
+                      <i className='fab fa-linkedin-in'></i>
+                    </a>
+                    <a href='https://github.com/ABuarque' target='_blank'>
+                      <i className='fab fa-github'></i>
+                    </a>
+                    <a href='https://medium.com/@abuarquemf' target='_blank'>
+                      <i className='fab fa-medium-m'></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-6 col-lg-4 item' data-aos='fade-up'>
+                <div className='box'>
+                  <img className='rounded-circle' src={require('./../img/hugo.png')} />
+                  <h3 className='name'>Hugo Davi</h3>
+                  <p className='title'>Diretor de Operações</p>
+                  <div className='social'>
+                    <a href='https://www.facebook.com/hugo.hdado' target='_blank'>
+                      <i className='fab fa-facebook-f'></i>
+                    </a>
+                    <a href='https://www.instagram.com/hugodavi' target='_blank'>
+                      <i className='fab fa-instagram'></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/hugodavi' target='_blank'>
+                      <i className='fab fa-linkedin-in'></i>
+                    </a>
+                    <a href='https://github.com/HugoDavi' target='_blank'>
+                      <i className='fab fa-github'></i>
+                    </a>
+                    <a href='https://medium.com/@hugo.hdado' target='_blank'>
+                      <i className='fab fa-medium-m'></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-6 col-lg-4 item' data-aos='fade-up'>
+                <div className='box'>
+                  <img className='rounded-circle' src={require('./../img/malta.png')} />
+                  <h3 className='name'>Fellipe Malta</h3>
+                  <p className='title'>Head de Front-end</p>
+                  <div className='social'>
+                    <a href='https://www.facebook.com/fellipe.dm' target='_blank'>
+                      <i className='fab fa-facebook-f'></i>
+                    </a>
+                    <a href='https://www.instagram.com/fellipe.dm' target='_blank'>
+                      <i className='fab fa-instagram'></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/fellipedm' target='_blank'>
+                      <i className='fab fa-linkedin-in'></i>
+                    </a>
+                    <a href='https://github.com/fmmalta' target='_blank'>
+                      <i className='fab fa-github'></i>
+                    </a>
+                    <a href='https://medium.com/@fellipemalta' target='_blank'>
+                      <i className='fab fa-medium-m'></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-6 col-lg-4 item' data-aos='fade-up'>
+                <div className='box'>
+                  <img className='rounded-circle' src={require('./../img/flavio.png')} />
+                  <h3 className='name'>Flávio Farias</h3>
+                  <p className='title'>Head de Design</p>
+                  <div className='social'>
+                    <a href='https://www.facebook.com/flaviofariasjr' target='_blank'>
+                      <i className='fab fa-facebook-f'></i>
+                    </a>
+                    <a href='https://www.instagram.com/flavioaq2' target='_blank'>
+                      <i className='fab fa-instagram'></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/ffpsj' target='_blank'>
+                      <i className='fab fa-linkedin-in'></i>
+                    </a>
+                    <a href='https://github.com/ffpsj' target='_blank'>
+                      <i className='fab fa-github'></i>
+                    </a>
+                    <a href='https://medium.com/@ffpsj' target='_blank'>
+                      <i className='fab fa-medium-m'></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-6 col-lg-4 item' data-aos='fade-up'>
+                <div className='box'>
+                  <img className='rounded-circle' src={require('./../img/pedro.png')} />
+                  <h3 className='name'>Pedro Soares</h3>
+                  <p className='title'>Head de Back-end</p>
+                  <div className='social'>
+                    <a href='https://www.facebook.com/pedro.soares.58726' target='_blank'>
+                      <i className='fab fa-facebook-f'></i>
+                    </a>
+                    <a href='https://www.instagram.com/pedroh_soaresc' target='_blank'>
+                      <i className='fab fa-instagram'></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/pedrohsoares' target='_blank'>
+                      <i className='fab fa-linkedin-in'></i>
+                    </a>
+                    <a href='https://github.com/pedrohsoares' target='_blank'>
+                      <i className='fab fa-github'></i>
+                    </a>
+                    <a target='_blank'>
+                      <i className='fab fa-medium-m'></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AboutUs;
